e17a41ce7ee098748723507d56e31261a6f81e54
pho86@my.bcit.ca <pho86@my.bcit.ca>
2022-11-14 19:08:58 -0800

    initial godot files w base

---
+++ Timer.gd
@@ -0,0 +1,34 @@
+extends Control
+
+export (int) var minutes = 0
+export (int) var seconds = 0
+var dsec = 0
+
+func _physics_process(delta):
+	if seconds > 0 and dsec <= 0 :
+		seconds -= 1
+		dsec = 10
+	if minutes > 0 and seconds <= 0: 
+		minutes -= 1
+		seconds = 60
+	
+	if seconds >= 10:
+		$sec.set_text(str(seconds))
+	else:
+		$sec.set_text("0"+str(seconds))
+	if dsec >= 10:
+		$dsec.set_text(str(dsec))
+	else:
+		$dsec.set_text("0"+str(dsec))
+	if minutes >= 10:
+		$min.set_text(str(minutes))
+	else:
+		$min.set_text("0"+str(minutes))
+		
+
+
+func _on_Timer_timeout():
+	dsec -= 10
+
+func on_timeout():
+	get_tree().change_scene("res://project/scenes/GameOver.tscn")
