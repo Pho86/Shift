246414ccd6449195b4981ee3dfd3ef06bc45329d
pho86 <pho86@my.bcit.ca>
2022-11-22 13:11:27 -0800

    add timer functions

--- Player.gd
+++ Player.gd
@@ -5,7 +5,7 @@ const GRAVITY = 20
 const MAXFALLSPEED = 200
 const MAXSPEED = 250
 const JUMPFORCE = 400 
-const ACCELERATION = 3
+const ACCELERATION = 5
 
 var canDash = true
 var motion = Vector2()
@@ -52,40 +52,12 @@ func _physics_process(_delta):
 		$AnimatedSprite.play("Idle")
 		motion.x = lerp(motion.x,0,0.2)
 		
-#	if canDash == false and motion.x <= 50:
-#		yield(get_tree().create_timer(1.0), "timeout")
-#		canDash = true
 		
-	if Input.is_action_just_pressed("dash") and canDash:
+	if Input.is_action_just_pressed("dash") and canDash and !is_on_floor():
 #		motion.x = clamp(motion.x, 1000, MAXSPEED)
 		motion.x = clamp(motion.x, 1000, 1000)
 		motion.x = direction.x * 5000
 		canDash = false
-		print(motion)
-		
-#	elif Input.is_action_just_pressed("dash") and !canDash:
-#		print(motion)
-#		motion = direction.normalized() * 5000
-#		motion.x = clamp(motion.x, -MAXSPEED, MAXSPEED)
-#		motion = direction.normalized() / 250
-		
-		
-#			yield(get_tree().create_timer(0), "timeout")
-#			canDash = false
-#			get_tree().create_timer(5.0).connect("timeout", self, "set_timing", [false])
-#			canDash = true
-			
-#			yield(get_tree().create_timer(1.0),"timeout")
-#		elif facingRight == false:
-#			motion.x += -5000
-#		motion.x += 1000
-#		yield(get_tree().create_timer(1.0), "timeout")
-#		canDash = true
-#		motion.x += -1000
-#		canDash = false
-#		yield(get_tree().create_timer(1), "timeout")
-#		canDash = true
-		
 		
 		
 	if is_on_floor():
