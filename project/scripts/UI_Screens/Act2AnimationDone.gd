0c9812b36921244c7ce2e4ef8730b8f0ddfb9dad
pho86@my.bcit.ca <pho86@my.bcit.ca>
2022-12-09 20:28:39 -0800

    move transition screens and redo timeline

--- project/scripts/UI_Screens/Act2AnimationDone.gd
+++ project/scripts/UI_Screens/Act2AnimationDone.gd
@@ -2,4 +2,4 @@ extends AnimationPlayer
 
 func _on_AnimationPlayer_animation_finished(_anim_name):
 	# warning-ignore:return_value_discarded
-	get_tree().change_scene("res://project/scenes/Intro.tscn")
+	get_tree().change_scene("res://project/scenes/AfterLevel4.tscn")
