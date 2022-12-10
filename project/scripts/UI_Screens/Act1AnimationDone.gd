38452f928d5c59134685cc3d8a6cc9c862553e24
pho86@my.bcit.ca <pho86@my.bcit.ca>
2022-12-09 19:34:38 -0800

    add more lines to level3/4

--- project/scripts/UI_Screens/Act1AnimationDone.gd
+++ project/scripts/UI_Screens/Act1AnimationDone.gd
@@ -2,6 +2,6 @@ extends AnimationPlayer
 
 
 
-func _on_AnimationPlayer_animation_finished(anim_name):
+func _on_AnimationPlayer_animation_finished(_anim_name):
 	# warning-ignore:return_value_discarded
 	get_tree().change_scene("res://project/scenes/Intro.tscn")
